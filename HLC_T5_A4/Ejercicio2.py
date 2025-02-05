ea7fbb5797152bb7473e33a3468870bb974f4df4
mmormor <moralesmorillamanuel10@gmail.com>
2025-02-05 13:15:06 +0100

    Update Ejercicio2.py

--- HLC_T5_A4/Ejercicio2.py
+++ HLC_T5_A4/Ejercicio2.py
@@ -1,9 +1,11 @@
 def n_esimo(n):
-    a=0
-    b=1
-    for i in range(n-1):
-        c=a+b
-        a=b
-        b=c
-    print(c)
-n_esimo(6)
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    else:
+        return n_esimo(n-1)+n_esimo(n-2)
+   
+n=int(input("Introduce un numero: "))
+resultado=n_esimo(n)
+print(resultado)
