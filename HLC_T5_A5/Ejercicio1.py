f0ebfda3613711ae26b3c9bfcffca6ec95995be5
mmormor <moralesmorillamanuel10@gmail.com>
2025-02-21 14:07:28 +0100

    Update Ejercicio1.py

--- HLC_T5_A5/Ejercicio1.py
+++ HLC_T5_A5/Ejercicio1.py
@@ -1,12 +1,12 @@
-class Persona:
+class Personas:
     def __init__(self, nombre, edad, profesion):
         self.nombre = nombre
         self.edad = edad
         self.profesion = profesion
 
-    def presentarse(self):
+    def presentar(self):
         return f"Hola, mi nombre es {self.nombre}, tengo {self.edad} años y soy {self.profesion}."
 
 
-p = Persona("Ana", 28, "Ingeniera")
-print(p.presentarse())
+p = Personas("Ana", 28, "Ingeniera")
+print(p.presentar())
