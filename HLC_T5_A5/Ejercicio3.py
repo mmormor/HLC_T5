e14e17213f235a57bfa2ff23fb2d3c42cbe6c903
mmormor <moralesmorillamanuel10@gmail.com>
2025-02-21 14:15:42 +0100

    Update Ejercicio3.py

--- HLC_T5_A5/Ejercicio3.py
+++ HLC_T5_A5/Ejercicio3.py
@@ -1,16 +1,20 @@
 
-class Personas:
-    def __init__(self, nombre, edad, profesion):
+class Individuo:
+    def __init__(self, nombre, edad, ocupacion):
         self.nombre = nombre
         self.edad = edad
-        self.profesion = profesion
-    def presentarse(self):
-        return f"Hola, mi nombre es {self.nombre}, tengo {self.edad} años y soy {self.profesion}"
-class Trabajador(Personas):
-    def __init__(self, nombre, edad, profesion, empresa):
-        super().__init__(nombre, edad, profesion)
-        self.empresa = empresa
-    def presentar(self):
-        return f"Hola, mi nombre es {self.nombre}, tengo {self.edad} años, soy {self.profesion} y trabajo en {self.empresa}"
-p = Trabajador('Alejandro', 20, 'Informático', 'Simire2')
-print(p.presentar())
+        self.ocupacion = ocupacion
+
+    def descripcion(self):
+        return f"Hola, soy {self.nombre}, tengo {self.edad} años y me dedico a {self.ocupacion}"
+
+class Empleado(Individuo):
+    def __init__(self, nombre, edad, ocupacion, compania):
+        super().__init__(nombre, edad, ocupacion)
+        self.compania = compania
+
+    def informacion(self):
+        return f"Hola, soy {self.nombre}, tengo {self.edad} años, trabajo como {self.ocupacion} en {self.compania}"
+
+persona = Empleado("Alejandro", 20, "Informático", "Simire2")
+print(persona.informacion())
