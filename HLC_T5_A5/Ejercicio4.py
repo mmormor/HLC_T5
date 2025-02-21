11c88a0429f28142276c8cb279cfaf7e29e4477d
mmormor <moralesmorillamanuel10@gmail.com>
2025-02-21 14:06:54 +0100

    Update Ejercicio4.py

--- HLC_T5_A5/Ejercicio4.py
+++ HLC_T5_A5/Ejercicio4.py
@@ -1,6 +1,15 @@
-class CuentaBancaria:
+class CuentBanca:
+    def __init__(self, saldo):
+        self.saldo = saldo
+    def deposito(self, sumar):
+        self.sumar = sumar
+        self.saldo += self.sumar
+    def ver_sal(self):
+        return f"El saldo es de {self.saldo}"
+    def retir(self, restar):
+        self.restar = restar
         self.saldo -= self.restar
-cuenta = CuentaBancaria(1000)
-cuenta.depositar(500)
-cuenta.retirar(300)
-print(cuenta.ver_saldo())
+cuenta = CuentBanca(1000)
+cuenta.deposito(500)
+cuenta.retir(300)
+print(cuenta.ver_sal())
