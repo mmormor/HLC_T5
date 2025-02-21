18b8ce40ff20a031cc24fda0e3d71e7163089d5f
mmormor <moralesmorillamanuel10@gmail.com>
2025-02-21 14:07:17 +0100

    Update Ejercicio2.py

--- HLC_T5_A5/Ejercicio2.py
+++ HLC_T5_A5/Ejercicio2.py
@@ -1,15 +1,15 @@
-class Persona:
+class Personas:
     def __init__(self, nombre, edad, profesion):
         self.nombre = nombre
         self.edad = edad
         self.profesion = profesion
-    def presentarse(self):
+    def presentar(self):
         return f"Hola, mi nombre es {self.nombre}, tengo {self.edad} años y soy {self.profesion}"
-class Estudiante(Persona):
+class Estudiante(Personas):
     def __init__(self, nombre, edad, profesion, grado):
         super().__init__(nombre, edad, profesion)
         self.grado = grado
-    def presentarse(self):
+    def presentar(self):
         return f"Hola, mi nombre es {self.nombre}, tengo {self.edad} años, soy {self.profesion} y estudio {self.grado}"
 p = Estudiante('Alejandro', 20, 'Informático', 'SMR')
-print(p.presentarse())
+print(p.presentar())
